b5c5b69818c6ff1c632e1a40bcff560074dc2d99
s-1310 <sgyt10@gmail.com>
2020-06-30 00:13:55 +0530

    implementing java script with form p1 and main.js files

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+var description = document.querySelector('p');
+description.textContent = 'JavaScript itself is relatively compact, yet very flexible.Developers have written a variety of tools on top of the core JavaScript language, unlocking a vast amount of functionality with minimum effort';
+var a = 10; var b = 20;
+console.log(a,b);
+l = [1,2,3,4,5];
+document.getElementById('demo').innerHTML = String(l[3]);
+
+// var tbody  = document.getElementsByTagName('tbody')[0];
+// var total =0;
+// function getAvg(){
+
+//     subno = tbody.rows.length -2;
+//     for (var i=0; i < subno ;i++){
+//         total += Number(tbody.rows.item(i).cells.item(1).innerHTML);
+//     }
+//     return total/subno;
+// }
+
+// function getTotal(){
+//     return total.toString()
+// }
+
+// console.log(tbody.rows.item(1));
+// console.log(tbody.rows.)
+
+
+
